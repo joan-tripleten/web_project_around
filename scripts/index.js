33f482f5e5c0d699d9b84bcc466f75b6475b0fe3
keizer-37 <budate_@hotmail.com>
2025-08-19 00:11:46 -0600

    Sprint 7.5

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,24 @@
 // popup
 
-const edit = document.querySelector('.profile__edit')
-const popup = document.querySelector('.popup')
-const cross = document.querySelector('.popup__cross')
+const edit = document.querySelector(".profile__edit");
+const popup = document.querySelector(".popup");
+const cross = document.querySelector(".popup__cross");
 
 function open() {
   // popup.classList.toggle('popup_opened')
-  popup.classList.add('popup_opened')
+  popup.classList.add("popup_opened");
 }
 
 function close() {
-  popup.classList.remove('popup_opened')
+  popup.classList.remove("popup_opened");
 }
 
-edit.addEventListener('click', open)
-cross.addEventListener('click', close)
+edit.addEventListener("click", open);
+cross.addEventListener("click", close);
 
 // save
 
-let formElement = document.querySelector('.form__profile')
+let formElement = document.querySelector(".form");
 
 // Lo siguiente es el manipulador (handler) de entrega de formularios, aunque
 // no se enviará en ningún sitio todavía
@@ -26,34 +26,32 @@ let formElement = document.querySelector('.form__profile')
 // Observa que el nombre de la función comienza con un verbo
 // y describe exactamente lo que hace la función
 function handleProfileFormSubmit(evt) {
-    // Esta línea impide que el navegador
-    // entregue el formulario en su forma predeterminada.
-    evt.preventDefault();
-    // Una vez hecho esto, podemos definir nuestra propia forma de entregar el formulario.
-    // Lo explicaremos todo con más detalle después.
+  // Esta línea impide que el navegador
+  // entregue el formulario en su forma predeterminada.
+  evt.preventDefault();
+  // Una vez hecho esto, podemos definir nuestra propia forma de entregar el formulario.
+  // Lo explicaremos todo con más detalle después.
 
-    // Busquemos los campos del formulario en el DOM
-    const nameInput = document.querySelector('#name')
-    const jobInput = document.querySelector('#organisation-title')
+  // Busquemos los campos del formulario en el DOM
+  const nameInput = document.querySelector("#name");
+  const jobInput = document.querySelector("#organisation-title");
 
-    // Obtén los valores de cada campo desde la propiedad de valor correspondiente
-    const nameValue = nameInput.value
-    const jobValue = jobInput.value
+  // Obtén los valores de cada campo desde la propiedad de valor correspondiente
+  const nameValue = nameInput.value;
+  const jobValue = jobInput.value;
 
-    // Selecciona los elementos donde se introducirán los valores de los campos
-    const name = document.querySelector('.profile__name')
-    const about = document.querySelector('.profile__about')
+  // Selecciona los elementos donde se introducirán los valores de los campos
+  const name = document.querySelector(".profile__name");
+  const about = document.querySelector(".profile__about");
 
-    // Inserta nuevos valores utilizando el textContent
-    // propiedad del método querySelector()
-    name.textContent = nameValue
-    about.textContent = jobValue
+  // Inserta nuevos valores utilizando el textContent
+  // propiedad del método querySelector()
+  name.textContent = nameValue;
+  about.textContent = jobValue;
 
-    close()
+  close();
 }
 
 // Conecta el manipulador (handler) al formulario:
 // se observará el evento de entrega
-formElement.addEventListener('submit', handleProfileFormSubmit);
-
-
+formElement.addEventListener("submit", handleProfileFormSubmit);
