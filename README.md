a938abfc936f336f0aee5eb959ad5197a9dba8a9
keizer-37 <budate_@hotmail.com>
2025-08-18 23:11:18 -0600

    Sprint 7.1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Tripleten web_project_around
+
+Nombre del proyecto: Sprint 7.
+Descripción: En este proyecto se utilizó por primera vez JavaScript de manera oficial.
+Planes de mejor del proyecto:  Quería agregar unas cosas más como inhabilitar el botón cuando no hubiera texto; lo haré en el siguiente cuando tenga más conocimientos.
+https://joan-tripleten.github.io/web_project_around
---
+++ scripts/script.js
@@ -0,0 +1,59 @@
+// popup
+
+const edit = document.querySelector('.profile__edit')
+const popup = document.querySelector('.popup')
+const cross = document.querySelector('.popup__cross')
+
+function open() {
+  // popup.classList.toggle('popup_opened')
+  popup.classList.add('popup_opened')
+}
+
+function close() {
+  popup.classList.remove('popup_opened')
+}
+
+edit.addEventListener('click', open)
+cross.addEventListener('click', close)
+
+// save
+
+let formElement = document.querySelector('.form__profile')
+
+// Lo siguiente es el manipulador (handler) de entrega de formularios, aunque
+// no se enviará en ningún sitio todavía
+
+// Observa que el nombre de la función comienza con un verbo
+// y describe exactamente lo que hace la función
+function handleProfileFormSubmit(evt) {
+    // Esta línea impide que el navegador
+    // entregue el formulario en su forma predeterminada.
+    evt.preventDefault();
+    // Una vez hecho esto, podemos definir nuestra propia forma de entregar el formulario.
+    // Lo explicaremos todo con más detalle después.
+
+    // Busquemos los campos del formulario en el DOM
+    const nameInput = document.querySelector('#name')
+    const jobInput = document.querySelector('#organisation-title')
+
+    // Obtén los valores de cada campo desde la propiedad de valor correspondiente
+    const nameValue = nameInput.value
+    const jobValue = jobInput.value
+
+    // Selecciona los elementos donde se introducirán los valores de los campos
+    const name = document.querySelector('.profile__name')
+    const about = document.querySelector('.profile__about')
+
+    // Inserta nuevos valores utilizando el textContent
+    // propiedad del método querySelector()
+    name.textContent = nameValue
+    about.textContent = jobValue
+
+    close()
+}
+
+// Conecta el manipulador (handler) al formulario:
+// se observará el evento de entrega
+formElement.addEventListener('submit', handleProfileFormSubmit);
+
+
